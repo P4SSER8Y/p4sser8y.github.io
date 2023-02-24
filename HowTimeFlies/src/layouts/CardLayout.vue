73e07e47391c38bca7bf8485d51042720c0f1dd8
P4SSER8Y <P4SSER8Y@outlook.com>
2023-02-25 00:35:21 +0800

    ENH: use grid layout instead of masonic layout

--- HowTimeFlies/src/layouts/CardLayout.vue
+++ HowTimeFlies/src/layouts/CardLayout.vue
@@ -1,37 +1,50 @@
 <template>
-    <VueFlexWaterfall :col="col" align-content="center" ref="main">
+    <div class="wrapper" ref="main">
         <PreviewCard
-            :style="'margin: ' + props.margin + 'px'"
             v-for="(item, index) in sortedData"
             :data="item"
             :width="elementWidth"
             :key="index"
+            class="item"
         >
         </PreviewCard>
-    </VueFlexWaterfall>
+    </div>
 </template>
+
+<style scoped>
+.wrapper {
+    display: grid;
+    grid-template-columns: repeat(v-bind(col), v-bind(cssElementWidth));
+    grid-gap: v-bind(cssGap);
+}
+
+.item {
+    margin: v-bind(cssGap);
+}
+</style>
 
 <script setup lang="ts">
 import { Record, get_latest_timestamp } from 'src/components/models';
-import { VueFlexWaterfall } from 'vue-flex-waterfall';
 import PreviewCard from 'src/components/PreviewCard.vue';
 import { computed, ref, onMounted, onUnmounted } from 'vue';
 
 const props = withDefaults(
     defineProps<{
         data: Record[];
-        margin?: number;
+        gap?: number;
         maxElementWidth?: number;
     }>(),
     {
-        margin: 2,
+        gap: 2,
         maxElementWidth: 256,
     }
 );
 
 const elementWidth = ref(256);
-const main = ref<null | VueFlexWaterfall>(null);
+const main = ref<null | Element>(null);
 const col = ref(2);
+const cssGap = computed(() => props.gap + 'px');
+const cssElementWidth = computed(() => elementWidth.value + 'px');
 
 const sortedData = computed(() => {
     let temp = props.data;
@@ -50,14 +63,14 @@ onUnmounted(() => {
 });
 
 function foobar() {
-    let clientWidth = main.value?.$el.clientWidth * 0.95;
-    let cellWidth = props.maxElementWidth + 2 * props.margin;
+    let clientWidth = main.value?.clientWidth ?? 0 * 0.95;
+    let cellWidth = props.maxElementWidth + 2 * props.gap;
     if (clientWidth > 2 * cellWidth) {
         col.value = Math.round(clientWidth / cellWidth);
         elementWidth.value = clientWidth / col.value;
     } else {
         col.value = 2;
-        elementWidth.value = (clientWidth - col.value * props.margin * 2) / 2;
+        elementWidth.value = (clientWidth - col.value * props.gap * 2) / 2;
     }
 }
 </script>
