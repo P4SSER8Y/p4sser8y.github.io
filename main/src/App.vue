322b8a39b30fd8f5c443b9abf5ad3b2789552477
P4SSER8Y <P4SSER8Y@outlook.com>
2023-03-25 19:21:37 +0800

    NEW: add convey game to homepage

--- main/src/App.vue
+++ main/src/App.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import MorseCodeTitle from './MorseCodeTitle.vue';
+import ConveyGameOfLife from './ConveyGameOfLife.vue';
 </script>
 
 <template>
-  <MorseCodeTitle title="hello world"></MorseCodeTitle>
+  <!-- <MorseCodeTitle title="hello world"></MorseCodeTitle> -->
   <div class="grid grid-cols-1 grid-rows-1 h-screen w-screen">
-    <span class="grid grid-cols-2 gap-4 place-self-center align-self-center">
-      <button class="btn w-full" onclick="window.location.href='/stream'">stream</button>
-      <!-- <button class="btn w-full" onclick="window.location.href='/log'">log</button> -->
-    </span>
+    <div class="flex flex-col justify-center gap-4">
+      <ConveyGameOfLife :width="37" :height="21" class="place-self-end self-center"></ConveyGameOfLife>
+      <span class="flex flex-row place-self-center self-center">
+        <button class="btn w-full" onclick="window.location.href='/stream'">stream</button>
+      </span>
+    </div>
   </div>
 </template>
+
+<style scoped lang="postcss">
+@supports (-webkit-touch-callout: none) {
+  .h-screen {
+    height: -webkit-fill-available;
+  }
+}
+</style>
---
+++ main/src/ConveyGameOfLife.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="main">
+    <span v-for="i in count" :class="(state[i] == 1 ? 'block-on' : 'block-off') + ' block'"></span>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed, reactive, onMounted, onUnmounted } from 'vue';
+const props = defineProps<{ width: number; height: number }>();
+const count = ref(0);
+let timer: number = undefined;
+let a: Array<number> = undefined;
+let b: Array<number> = undefined;
+let state: Array<number> = undefined;
+
+onMounted(() => {
+  initMap();
+  timer = setInterval(step, 250);
+});
+
+onUnmounted(() => {
+  clearInterval(timer);
+  timer = undefined;
+});
+
+function index(x: number, y: number) {
+  x = (x + props.width) % props.width;
+  y = (y + props.height) % props.height;
+  return y * props.width + x;
+}
+
+function initMap() {
+  count.value = props.width * props.height;
+  a = new Array<number>(count.value);
+  b = new Array<number>(count.value);
+  state = reactive(new Array<number>(count.value));
+  for (let y = 0; y < props.height; y += 1) {
+    for (let x = 0; x < props.width; x += 1) {
+      b[index(x, y)] = Math.random() < 0.5 ? 1 : 0;
+    }
+  }
+  step();
+}
+
+function neightborCount(x: number, y: number) {
+  const MAP = [
+    [-1, -1],
+    [-1, 0],
+    [-1, +1],
+    [0, -1],
+    [0, +1],
+    [+1, -1],
+    [+1, 0],
+    [+1, +1],
+  ];
+  let sum = 0;
+  for (let v of MAP) {
+    sum += a[index(x + v[0], y + v[1])];
+  }
+  return sum;
+}
+
+function step() {
+  let t = a;
+  a = b;
+  b = t;
+  for (let x = 0; x < props.width; x++) {
+    for (let y = 0; y < props.height; y++) {
+      let idx = index(x, y);
+      let me = a[idx];
+      let n = neightborCount(x, y);
+      if (me == 0) {
+        b[idx] = n == 3 ? 1 : 0;
+      } else {
+        b[idx] = n == 2 || n == 3 ? 1 : 0;
+      }
+      state[idx] = b[idx];
+    }
+  }
+}
+</script>
+
+<style scoped lang="postcss">
+.main {
+  @apply inline-grid gap-1 content-center;
+  grid-template-rows: repeat(v-bind('props.height'), minmax(1px, 1fr));
+  grid-template-columns: repeat(v-bind('props.width'), minmax(1px, 1fr));
+}
+
+.block {
+  @apply transition duration-150 align-middle inline-block w-2 h-2 md:w-4 md:h-4;
+  aspect-ratio: 1;
+}
+
+.block-on {
+  @apply bg-secondary;
+}
+
+.block-off {
+  @apply bg-neutral;
+}
+</style>
