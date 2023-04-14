84dd93e6a843ed3fb280c7f112c205b12a305c1c
P4SSER8Y <P4SSER8Y@outlook.com>
2023-04-15 01:15:50 +0800

    NEW: add link to moegirl.org.cn

--- stream/src/components/DatabaseLink.vue
+++ stream/src/components/DatabaseLink.vue
@@ -1,7 +1,8 @@
 <template>
     <a v-if="type" :href="href">
         <q-avatar rounded size="sm">
-            <IconFont :name="type.icon" width="100%"></IconFont>
+            <IconFont v-if="type.type == IconType.iconFont" :name="type.icon" width="100%"></IconFont>
+            <img v-if="type.type == IconType.img" :src="type.icon" width="100%">
         </q-avatar>
     </a>
 </template>
@@ -10,11 +11,17 @@
 import IconFont from 'src/assets/IconFont.vue';
 import { computed } from 'vue';
 
+enum IconType {
+    iconFont,
+    img,
+}
+
 interface Type {
     abbr: string;
     hrefPrefix: string;
     hrefSuffix: string;
     icon: string;
+    type: IconType;
 }
 
 const WTF: Type[] = [
@@ -23,12 +30,21 @@ const WTF: Type[] = [
         icon: 'icon-douban',
         hrefPrefix: 'https://movie.douban.com/subject/',
         hrefSuffix: '/',
+        type: IconType.iconFont,
     },
     {
         abbr: 'imdb:',
         icon: 'icon-imdb',
         hrefPrefix: 'https://www.imdb.com/title/',
         hrefSuffix: '/',
+        type: IconType.iconFont,
+    },
+    {
+        abbr: 'moe:',
+        icon: 'assets/moegirl.ico',
+        hrefPrefix: 'https://zh.moegirl.org.cn/',
+        hrefSuffix: '',
+        type: IconType.img,
     },
 ];
 
@@ -48,8 +64,8 @@ const type = computed(() => {
 const href = computed(() =>
     type.value
         ? type.value.hrefPrefix +
-          props.link.slice(type.value.abbr.length) +
-          type.value.hrefSuffix
+        props.link.slice(type.value.abbr.length) +
+        type.value.hrefSuffix
         : undefined
 );
 </script>
