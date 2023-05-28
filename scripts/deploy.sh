362906c99500365a54c1c09737bdeb113ca2e871
P4SSER8Y <P4SSER8Y@outlook.com>
2023-05-28 16:25:05 +0800

    FIX: add install step to deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 set -e
+node -v
 
 root=$(dirname $0)/..
 cd $root
@@ -10,12 +11,19 @@ echo root=$root
 echo output_directory=$outdir
 
 cd $root/main
+if [ -n "${CI+1}" ]; then
+    yarn
+fi
 yarn build
 mkdir -p $outdir/
 cp -r $root/main/dist/* $outdir/
 
 cd $root/stream
-quasar build
+if [ -n "${CI+1}" ]; then
+    yarn global add @quasar/cli
+    yarn
+fi
+yarn quasar build
 yarn data
 mkdir -p $outdir/stream
 cp -r $root/stream/dist/* $outdir/stream
