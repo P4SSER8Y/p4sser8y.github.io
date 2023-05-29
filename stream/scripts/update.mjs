c3d145c3e867bdb2ba138f8adb73f14054dca891
P4SSER8Y <P4SSER8Y@outlook.com>
2023-05-30 00:56:23 +0800

    NEW: change file type to md for CMS

--- stream/scripts/update.mjs
+++ stream/scripts/update.mjs
@@ -22,16 +22,44 @@ console.log(`copy "${src}" to "${dst}`);
 let full = [];
 let files = readdirSync(src, { withFileTypes: true })
     .filter((x) => x.isFile())
-    .filter((x) => x.name.toLowerCase().endsWith('.yml'));
+    .filter((x) => x.name.toLowerCase().endsWith('.md'));
 for (let file of files) {
     let path = join(src, file.name);
+    console.log(`processing ${path}`);
     let raw = readFileSync(path, { encoding: 'utf8' });
-    let datas = parseAllDocuments(raw);
+    let yml = '';
+    let step = 0;
+    let separator = '';
+    for (let line of raw.split(/[\r\n]+/)) {
+        switch (step) {
+            case 0:
+                if (line.trim().match(/^-{3,}\s*$/)) {
+                    step = 1;
+                    separator = line.trim();
+                }
+                break;
+            case 1:
+                if (line.trim() == separator) {
+                    step = 2;
+                    break;
+                }
+                yml += line + '\n';
+                break;
+        }
+        if (step == 2) {
+            break;
+        }
+    }
+    if (step != 2) {
+        yml = raw;
+    }
+    let datas = parseAllDocuments(yml);
     datas
         .map((x) => x.toJS())
         .filter((x) => x)
         .map((x) => full.push(x));
 }
+console.log(`found ${full.length} records`);
 
 removeSync(dst);
 mkdirSync(dst, { recursive: true, mode: 0o777 });
